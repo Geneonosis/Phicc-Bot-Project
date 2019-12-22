8c5fe8f36ae66f198fb7ea2b51a68a55b7cee364
geneonosis <genetigner.art@gmail.com>
2019-12-22 01:31:20 -0600

    created a usage.json file
    created a usage.json and transfered some content from "config.json" to separate security type items from functionality items.
    
    expanded the respones array in json.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
-const config = require('./config.json');
+//const config = require('./config.json');
+const {token} = require('./config.json')
+const {prefix, responses} = require('./usage.json')
 
 const client = new Discord.Client();
 
@@ -8,10 +10,19 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-    //console.log(message.content);
-    if(message.content === config.prefix + 'phicc'){
-        message.channel.send('Quack!');
-    }
+    
+    //If the message starts with '!phicc' then the
+    //user is addressing the bot (me) and I need
+    //to respond to the user with one of my many
+    //dialogue options
+    if(message.content.startsWith(`${prefix}phicc`)){
+        
+        message.channel.send(responses[getRandomInt(Object.keys(responses).length)]);
+    } 
 });
 
-client.login(config.token);
+function getRandomInt(max){
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+client.login(token);
